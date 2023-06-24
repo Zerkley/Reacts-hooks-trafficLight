8c1c6ee6d312f3db6dd2838c97cfba8b687b0eb8
Zerkley <miguelmr94@gmail.com>
2023-06-24 20:32:27 +0100

    Final ver

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,56 @@
-import React from "react";
-import RedLight from "./redLight";
-import YellowLight from "./yellowLight";
-import GreenLight from "./greenLight";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
   //
+  const [glowRed, setGlowRed] = useState("null");
+  const [glowYellow, setGlowYellow] = useState("null");
+  const [glowGreen, setGlowGreen] = useState("null");
+
+  const yellowClick = () => {
+    setGlowYellow("selected");
+    setGlowRed("null");
+    setGlowGreen("null");
+  };
+  const redClick = () => {
+    setGlowYellow("null");
+    setGlowRed("selected");
+    setGlowGreen("null");
+  };
+  const greenClick = () => {
+    setGlowYellow("null");
+    setGlowRed("null");
+    setGlowGreen("selected");
+  };
 
   return (
     <div>
       <div
         className="container d-flex flex-column bg-black"
         style={{ width: "1rem", height: "15rem" }}
-      >
-        iii
-      </div>
+      ></div>
       <div
         className="container d-flex flex-column justify-content-center bg-black"
         style={{ width: "9rem", height: "20rem" }}
       >
-        <RedLight />
-        <YellowLight />
-        <GreenLight />
+        <button
+          className={`RedLight ${glowRed} rounded-circle mx-4 my-2`}
+          id="redLight"
+          style={{ width: "5rem", height: "5rem" }}
+          onClick={redClick}
+        ></button>
+        <button
+          className={`YellowLight ${glowYellow} rounded-circle mx-4 my-2`}
+          id="yellowLight"
+          style={{ width: "5rem", height: "5rem" }}
+          onClick={yellowClick}
+        ></button>
+        <button
+          className={`GreenLight ${glowGreen} rounded-circle mx-4 my-2`}
+          id="greenLight"
+          style={{ width: "5rem", height: "5rem" }}
+          onClick={greenClick}
+        ></button>
       </div>
     </div>
   );
